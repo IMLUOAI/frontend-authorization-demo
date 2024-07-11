6c58fc5a7a0c9bb36242afe9fa74f9a9e7fd7490
IMLUOAI <imluoai@gmail.com>
2024-07-10 20:40:03 -0500

    feat. new files and codes were added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,50 @@
-import { Routes, Route } from "react-router-dom";
+
+import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Routes>
-      <Route path="/ducks" element={<Ducks />} />
-      <Route path="/my-profile" element={<MyProfile />} />
-      <Route
-        path="/login"
-        element={
-          <div className="loginContainer">
-            <Login />
-          </div>
-        }
+      <Route 
+      path="/ducks" 
+      element={
+        <ProtectedRoute isLoggedIn={isLoggedIn}>
+          <Ducks />
+      </ProtectedRoute>
+      } 
+      />
+      <Route 
+      path="/my-profile" 
+      element={
+        <ProtectedRoute isLoggedIn={isLoggedIn}>
+          <MyProfile />
+        </ProtectedRoute>
+      } 
+      />
+      <Route 
+      path="/login" 
+      element={
+        // <ProtectedRoute isLoggedIn={isLoggedIn}>
+          <Login />
+      //  </ProtectedRoute>
+      } 
       />
       <Route
-        path="/register"
+       path="/register"
         element={
-          <div className="registerContainer">
-            <Register />
-          </div>
+        // <ProtectedRoute isLoggedIn={isLoggedIn}>
+          <Register />
+        // </ProtectedRoute>
         }
-      />
+         />  
     </Routes>
   );
 }
